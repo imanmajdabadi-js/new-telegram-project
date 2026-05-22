db6694b076511cc8241b6b34c8ec1819b8ac6ab9
Iman Majd <imanmajdabadifarahani1372@gmail.com>
2026-05-22 17:24:01 +0330

    end

--- src/components/PhoneBook.tsx
+++ src/components/PhoneBook.tsx
@@ -4,6 +4,7 @@ interface User {
   firstName: string;
   lastName: string;
   number: string;
+  id?: string;
 }
 
 const PhoneBook = () => {
@@ -23,24 +24,31 @@ const PhoneBook = () => {
 
   const handleSaveUser = (e: React.SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const newContact = { ...formPhone };
-    const updatedList = [...list, newContact];
-    setList(updatedList);
-    localStorage.setItem('myContacts', JSON.stringify(updatedList));
+    if (formPhone.firstName !== '' || formPhone.lastName !== '' || formPhone.number !== '') {
+      const newContact = { ...formPhone };
+      const updatedList = [...list, newContact];
+      setList(updatedList);
+      localStorage.setItem('myContacts', JSON.stringify(updatedList));
+      setFormPhone({ firstName: '', lastName: '', number: '' });
+    }
   };
 
+  const handleDelete = (id: string) => {
+    const filterdList = list.filter((item) => item.id !== id);
+    localStorage.removeItem('myContacts');
+    const copy = [...filterdList];
+    setList(copy);
+  };
   return (
     <form onSubmit={handleSaveUser}>
       <div>
         <label htmlFor="firstName">Name</label>
         <input name="firstName" value={formPhone.firstName} onChange={handleChange} type="text" />
       </div>
-
       <div>
         <label htmlFor="lastName">Last name</label>
         <input value={formPhone.lastName} name="lastName" onChange={handleChange} type="text" />
       </div>
-
       <div>
         <label htmlFor="number">Number</label>
         <input value={formPhone.number} name="number" onChange={handleChange} type="number" />
@@ -56,10 +64,11 @@ const PhoneBook = () => {
       >
         Save
       </button>
-      <ul>
-        {list.map((contact, index) => (
-          <li key={index}>
-            {contact.firstName} {contact.lastName} — {contact.number}
+
+      <ul style={{ listStyle: 'none' }}>
+        {list.map((contact) => (
+          <li onClick={() => handleDelete(contact.id!)} key={contact.id}>
+            {contact.firstName} {contact.lastName} — {contact.number} _ {'X'}
           </li>
         ))}
       </ul>
